e4255204dee55ffa51d13ed20d6b133ed6c45047
milan.svitlica <milan.svitlica@leversys.com>
2020-01-05 17:22:44 +0100

    implemented border outline for selected buttons

--- main.js
+++ main.js
@@ -5,3 +5,23 @@ function selected(){
     }
     //document.getElementById(testName).classList.add('selected_button');
 }
+
+
+function onPageLoad() {
+    console.log(document.getElementsByClassName("button"));
+    const buttons = document.getElementsByClassName("button");
+    
+    for(let i=0; i< buttons.length; i++){
+        const button = buttons[i];
+        console.log(button);
+
+
+        button.addEventListener('click', (event) => {
+            const buttons = [...document.getElementsByClassName("button")].forEach(button => button.classList.remove("selected_button"));
+            
+            button.classList.add("selected_button");
+
+        })
+    }
+    
+}
