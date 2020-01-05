6e958de25c1fb39c987348623c8e674e4f92bce9
milan.svitlica <milan.svitlica@leversys.com>
2020-01-05 17:34:02 +0100

    removed unused code

--- main.js
+++ main.js
@@ -1,20 +1,10 @@
-function selected() {
-    let testName = document.getElementById('testHolder').getElementsByTagName('*');
-    for (let i = 0; i < testName.length; i++) {
-        console.log(testName[i])
-    }
-    //document.getElementById(testName).classList.add('selected_button');
-}
-
-
 function onPageLoad() {
-    [...document.getElementsByClassName("button")].forEach(button => {
-        button.addEventListener('click', (event) => {
+    [...document.getElementsByClassName("button")].forEach(button => 
+        {
+        button.addEventListener('click', (event) => 
+        {
             [...document.getElementsByClassName("button")].forEach(button => button.classList.remove("selected_button"));
-
             button.classList.add("selected_button");
-
         })
     });
-
 }
