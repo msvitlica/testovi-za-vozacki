23ea40e245df5ae664e7a16354eea588eb9313f7
NadaJankovic <58693023+NadaJankovic@users.noreply.github.com>
2020-01-07 15:44:53 +0100

    Add files via upload

---
+++ homePage.js
@@ -0,0 +1,86 @@
+var testButtonsB = [
+    {
+        name: 'Test1',
+        category: 'B'
+    },
+    {
+        name: 'Test2',
+        category: 'B'
+    },
+    {
+        name: 'Test3',
+        category: 'B'
+    },
+    {
+        name: 'Test4',
+        category: 'B'
+    },
+    {
+        name: 'Test5',
+        category: 'B'
+    },
+    {
+        name: 'Test6',
+        category: 'B'
+    },
+    {
+        name: 'Test7',
+        category: 'B'
+    },
+    {
+        name: 'Test9',
+        category: 'B'
+    },
+    {
+        name: 'Test10',
+        category: 'B'
+    },
+    {
+        name: 'Test11',
+        category: 'B'
+    },
+    {
+        name: 'Test12',
+        category: 'B'
+    },
+    {
+        name: 'Test13',
+        category: 'B'
+    }
+]
+var testButtonsC= [
+    {
+        name: 'Test14',
+        category: 'C'
+    },
+    {
+        name: 'Test15',
+        category: 'C'
+    },
+    {
+        name: 'Test16',
+        category: 'C'
+    }
+]
+var testButtonsD= [
+    {
+        name: 'Test17',
+        category: 'D'
+    },
+    {
+        name: 'Test18',
+        category: 'D'
+    },
+    {
+        name: 'Test19',
+        category: 'D'
+    },
+    {
+        name: 'Test20',
+        category: 'D'
+    }
+]
+function clickStartButton() {
+    alert(selectedTestButton);
+
+}
--- main.js
+++ main.js
@@ -1,17 +1,20 @@
-let selectedTest = undefined;
-
+var selectedTestButton = undefined;
 function onPageLoad() {
-    [...document.getElementsByClassName("button")].forEach(button => 
-        {
-        button.addEventListener('click', (event) => 
-        {
-            [...document.getElementsByClassName("button")].forEach(button => button.classList.remove("selected_button"));
-            button.classList.add("selected_button");
-            selectedTest = button.innerHTML;            
+    testButtonsB.forEach(el => {
+        document.getElementById('testHolder1').innerHTML += '<button class= "button">' + el.name + '</button>'
+    });
+    testButtonsC.forEach(el => {
+        document.getElementById('testHolder2').innerHTML += '<button class= "button">' + el.name + '</button>'
+    });
+    testButtonsD.forEach(el => {
+        document.getElementById('testHolder3').innerHTML += '<button class= "button">' + el.name + '</button>'
+    });
+    [...document.getElementsByClassName("button")].forEach(button => {
+        button.addEventListener('click', () => {
+            [...document.getElementsByClassName("button")].forEach(button =>
+                button.classList.remove("selected_button")),
+                button.classList.add("selected_button"),
+                selectedTestButton = button.innerHTML;
         })
     });
-}
-
-function onStart(){
-    alert(selectedTest);
 }
