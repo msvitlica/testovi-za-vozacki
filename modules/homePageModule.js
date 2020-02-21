55227ba5d71dfc46107f95dba5ed7be0f9d8020a
nottebianca <sreckolazic94@gmail.com>
2020-02-21 18:23:49 +0100

    trial version of Home Page Module

--- modules/homePageModule.js
+++ modules/homePageModule.js
@@ -1,5 +1,7 @@
 import { onPageLoad, clickStartButton } from '../homePage.js';
 
 const body = document.getElementById('body');
+const startBtn = document.getElementById('start');
 
+startBtn.addEventListener('click', clickStartButton);
 body.addEventListener('load', onPageLoad());
