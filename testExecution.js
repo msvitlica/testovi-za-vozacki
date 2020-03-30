c2c4c90a892e95aecf828fbb3137d276adf53938
nottebianca <sreckolazic94@gmail.com>
2020-03-30 17:42:39 +0200

    New code for questions and answers

--- testExecution.js
+++ testExecution.js
@@ -21,29 +21,53 @@ function loadTest() {
 
     test.questions.forEach((questionElement, questionIndex) => {
         let question = questionStorage.getQuestionById(questionElement.id);
-        let div = document.createElement('div');
 
         testContent.innerHTML += '<div id="question' + (questionIndex + 1) + '" class="questions"><p>' + questionElement.questionText + '</p></div>';
-        let answerContent = document.getElementById('question' + (questionIndex + 1)).appendChild(div);
-        answerContent.classList = 'answers' + questionElement.id;
+        let answerContent = document.getElementById('question' + (questionIndex + 1)).appendChild(document.createElement('div'));
+        answerContent.id = 'answersFor' + questionElement.id;
 
-        if (question.hasMultipleCorrectAnswers()) {
+        if (!question.hasMultipleCorrectAnswers()) {
             question.answers.forEach((answerElement, answerIndex) => {
-                answerContent.innerHTML += '<div><input type="checkbox" id="' + questionElement.id + '_answer_' + answerIndex + '" name="answer' + questionElement.id + '" value="' + answerElement.correct + '">< for="' + questionElement.id + '_answer_' + answerIndex + '">' + answerElement.answer + '</label></div>';
-                let eventHandler = document.getElementById(questionElement.id + '_answer_' + answerIndex);
-                eventHandler.addEventListener('change', () => { onAnswerClick(questionElement.id) });
+                let answerDiv = answerContent.appendChild(document.createElement('div'));
+                answerDiv.id = questionElement.id + 'answer' + answerIndex;
+                let radio = document.createElement('input');
+                let answerLabel = document.createElement('label');
+                radio.id = questionElement.id + '_answer_' + answerIndex;
+                radio.setAttribute('type', 'radio');
+                radio.setAttribute('value', answerElement.correct);
+                radio.setAttribute('name', questionElement.id + 'answer');
+                answerLabel.setAttribute('for', radio.id);
+                answerLabel.innerHTML = answerElement.answer;
+                answerDiv.appendChild(radio);
+                answerDiv.appendChild(answerLabel);
+                radio.addEventListener('change', () => { onAnswerClick(questionElement.id, radio.value) });
             });
+
         }
         else {
             question.answers.forEach((answerElement, answerIndex) => {
-                answerContent.innerHTML += '<div><input type="radio" id="' + questionElement.id + '_answer_' + answerIndex + '" name="answer' + questionElement.id + '" value="' + answerElement.correct + '"><label for="' + questionElement.id + '_answer_' + answerIndex + '">' + answerElement.answer + '</label></div>';
-                let eventHandler = document.getElementById(questionElement.id + '_answer_' + answerIndex);
-                eventHandler.addEventListener('change', () => { onAnswerClick(questionElement.id) });
+                let answerDiv = answerContent.appendChild(document.createElement('div'));
+                answerDiv.id = questionElement.id + 'answer' + answerIndex;
+                let checkbox = document.createElement('input');
+                let answerLabel = document.createElement('label');
+                checkbox.setAttribute('type', 'checkbox');
+                checkbox.setAttribute('value', answerElement.correct);
+                checkbox.id = questionElement.id + '_answer_' + answerIndex;
+                answerLabel.setAttribute('for', checkbox.id);
+                answerLabel.innerHTML = answerElement.answer;
+                answerDiv.appendChild(checkbox);
+                answerDiv.appendChild(answerLabel);
+                checkbox.addEventListener('change', () => { onAnswerClick(questionElement.id, checkbox.value) });
             });
         }
     });
 }
-function onAnswerClick(id) {
-    let question = questionStorage.getQuestionById(id);
-    console.log(question);
+function onAnswerClick(id, valueOfAnswer) {
+    questionsAnswers.addAnswer(
+        {
+            id: id,
+            correct: valueOfAnswer
+        }
+    )
+    console.log(questionsAnswers.answers);
 }
